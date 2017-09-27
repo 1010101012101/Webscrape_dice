bea4c0e5b0ace62eb85bcb4d37b291d7dcd984b8
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-27 18:30:19 +0800

    Update Simple_check.py

--- Simple_check.py
+++ Simple_check.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 import requests
 
-url = "www.google.com.sg/search?dcr=0&source=hp&q=python&oq=python&gs_l=psy-ab.3..0i131k1l3j0.6479.7949.0.8229.6.6.0.0.0.0.183.335.0j2.2.0....0...1.1.64.psy-ab..4.2.334....0.aT6b7NR6SS0"
+url = "github.com/GoogleCloudPlatform/reliable-task-scheduling-compute-engine-sample"
 
 r  = requests.get("https://" +url)
 
